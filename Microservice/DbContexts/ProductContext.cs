4dcaf71c1a14209a06bd55b480a34eb87f2477f8
Zohidjon-T <Ab0186883>
2020-11-01 10:35:30 +0500

    My First Commit

---
+++ Microservice/DbContexts/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microservice.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservice.DbContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ Microservice/Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microservice.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.Repository
+{
+    public interface IProductRepository
+    {
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+        Product GetProductById(int id);
+        IEnumerable<Product> GetProducts();
+
+
+    }
+}
